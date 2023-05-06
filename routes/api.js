b4c1dc3e4bf8142fd7c1eb6529a7c4f55ad6c4e1
Hasegawa-Akito <h.akito1999@gmail.com>
2023-05-06 21:37:56 +0900

    クロスオリジンからの単純リクエスト時のアクセス許可

--- routes/api.js
+++ routes/api.js
@@ -1,14 +1,31 @@
 const express = require("express");
 const router = express.Router();
 
+const allowOriginList = ["http://localhost:3000", "http://127.0.0.1:3000"];
+
 router.use(express.json());
+router.use((req, res, next) => {
+  const origin = req.headers.origin;
+  // Originヘッダが存在している、かつリクエストを許可するリスト内にOriginヘッダの値が含まれている
+  if (origin && allowOriginList.includes(origin)) {
+    // クロスオリジンからのリクエストを許可
+    res.header("Access-Control-Allow-Origin", origin);
+  }
+
+  // プリフライトリクエストの時のみAccess-Control-Allow-Headersを追加するようにする。
+  // プリフライトリクエストは基本的に初回のみ
+  // プリフライトリクエストmethodにはOPTIONSが使われる
+  if (req.method == "OPTIONS") {
+    res.header("Access-Control-Allow-Headers", "X-Token");
+  }
+  next();
+});
 
 router.get("/", (req, res) => {
   res.setHeader("X-Timestamp", Date.now());
 
   let message = req.query.message;
   const lang = req.headers["x-lang"];
-  console.log(lang);
 
   if (message == "") {
     res.status(400);
